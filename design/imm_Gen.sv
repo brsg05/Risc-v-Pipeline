671ab48075e187220ca87a04ef04a3f8e71e4d75
Breno Ramos <145413402+brsg05@users.noreply.github.com>
2024-02-27 17:49:37 -0300

    Added a JalR, Jal and I Type immediate generators.

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -8,8 +8,10 @@ module imm_Gen (
 
   always_comb
     case (inst_code[6:0])
-      7'b0000011:  /*I-type load part*/
+      7'b0010011:  /load part*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+                  //I-Type
+      7'b0000011: Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
 
       7'b0100011:  /*S-type*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
@@ -23,7 +25,13 @@ module imm_Gen (
         inst_code[11:8],
         1'b0
       };
+      
+        //Jal
+      7'b1101111: Imm_out = { {12{inst_code[31]}}, inst_code[31], inst_code[19:12], inst_code[20], inst_code[30:21], 1'b0};
 
+        //JalR
+      7'b1100111: Imm_out = { {20{inst_code[31]}}, inst_code[31:20]};
+      
       default: Imm_out = {32'b0};
 
     endcase
